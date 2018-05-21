db805eafb28da08dcb1f8e485f615b2dfd680ff5
yyh-gl <yhonda.95.gl@gmail.com>
2018-05-21 14:34:48 +0900

    TYC流通量を表示するように修正

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -10,18 +10,23 @@ class TransactionsController < ApplicationController
       @env = '開発'
     end
     csv_data = CSV.read(TRANSACTION_LOG_PATH, "r:ISO-8859-1")
-    @bank_data = []
+    bank_data = []
     @bank_data_list = []
+    tyc_amount = 0
+    @tyc_amount_list = []
     @transaction_data =[]
     count = 0
     csv_data.each do |data|
       if count % 2 == 0
-        @bank_data << data[0]
+        bank_data << data[0]
         data[1..-1].each_slice(2) do |a, b|
-          @bank_data << [a, b]
+          bank_data << [a, b]
+          tyc_amount += b.to_i
         end
-        @bank_data_list << @bank_data
-        @bank_data = []
+        @bank_data_list << bank_data
+        bank_data = []
+        @tyc_amount_list << tyc_amount
+        tyc_amount = 0
       else
         @transaction_data << data
       end
